0ca3ec94cfce0c59878e0461fcde086ed6904d84
anitaprabha.antony@cognizant.com <root@ip-10-0-0-83.us-west-2.compute.internal>
2015-08-10 03:12:37 -0400

    nagios test commit

--- cookbooks/nagios/metadata.rb
+++ cookbooks/nagios/metadata.rb
@@ -2,7 +2,7 @@ name              'nagios'
 maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
-description       'Installs and configures Nagios server and the NRPE client'
+description       'Installs and configures Nagios server and the NRPE client -1'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '5.0.2'
 
